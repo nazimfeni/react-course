c673678a16c807bd441b306a48a97a167d03b600
nazimfeni <mdnazimuddin77@gmail.com>
2025-12-16 22:21:24 +0600

    project created 02counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  // let counter = 14;
+
+  const [counter, setCounter] = useState(0);
+
+  const addValue = () => {
+    //setCounter(counter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+
+  const removeValue = () => {
+    setCounter(counter - 1);
+  };
+
+  return (
+    <>
+      <h1>React With Nazim {counter}</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={addValue}>Add Value</button>{" "}
+      <button onClick={removeValue}>Remove Value: </button>
+      <p>Footer: {counter}</p>
+    </>
+  );
+}
+
+export default App;
